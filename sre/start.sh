bd189ffda5c5d4cbe1d5ebd6fed1cd3acca653e3
Jim Shingler <shinglerjim@gmail.com>
2025-12-25 19:06:59 -0500

    jshingler/ShingNet#10 setup alloy to send metrics

--- sre/start.sh
+++ sre/start.sh
@@ -3,4 +3,5 @@ docker compose -f docker-compose.base.yaml \
                -f docker-compose.alertmanager.yaml \
                -f docker-compose.loki.yaml \
                -f docker-compose.tempo.yaml \
-               -f docker-compose.grafana.yaml up -d
+               -f docker-compose.grafana.yaml \
+	       -f docker-compose.alloy.yaml up -d
--- sre/stop.sh
+++ sre/stop.sh
@@ -3,4 +3,5 @@ docker compose -f docker-compose.base.yaml \
                -f docker-compose.alertmanager.yaml \
                -f docker-compose.loki.yaml \
                -f docker-compose.tempo.yaml \
-               -f docker-compose.grafana.yaml down
+               -f docker-compose.grafana.yaml \
+	       -f docker-compose.alloy.yaml down
